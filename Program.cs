fb74ff2192557fa36a4ba9463609715004caccc5
Jake Hiney <hiney.jake@outlook.com>
2021-06-02 15:38:08 -0500

    feat: full working parser with file select and output tested

--- Program.cs
+++ Program.cs
@@ -12,25 +12,29 @@ namespace concur_parser
         static void Main(string[] args)
         {
             string filePath = @"K:\ACCTING\GENERAL\Expense reports\Concur Extracts";
+            string previous = null;
             bool showMenu = true;         
             filePath = MainMenu(filePath);
+            
             while (showMenu)
-            {             
-                if (filePath.Length!=0)
-                {                   
+            {  
+                if (filePath.Length != 0 && !filePath.Equals("found"))
+                {
+                    previous = filePath;     
                     filePath = MainMenu(filePath);
                 }
                 else
                 {
-                    showMenu = false;                  
-                }                                                  
+                    showMenu = false;
+                }
             }
 
-            //Console.Write("Enter Month End Closing Date (YYYY-MM-DD): ");
+            Console.Write("Enter Month End Closing Date (YYYY-MM-DD): ");
             string newDate = Console.ReadLine();
+            
             List<string> newItems = new List<string>();
             
-            StreamReader reader = File.OpenText("C:\\Users\\jhiney\\source\\repos\\concur-parser\\test.txt");
+            StreamReader reader = File.OpenText(previous);
             string line;
 
             while ((line = reader.ReadLine()) != null)
@@ -45,33 +49,58 @@ namespace concur_parser
                     newItems.Add(string.Join('|', parts)); 
                 }
             }
-            //File.WriteAllLines("C:\\Users\\jhiney\\source\\repos\\concur-parser\\output.txt", newItems.ToArray());
+           File.WriteAllLines("C:\\Users\\jhiney\\source\\repos\\concur-parser\\output.txt", newItems.ToArray());
 
             Console.WriteLine("Done");
             Console.Read();
         }
 
         public static string MainMenu(string filePath) {
-         
-            string[] filePaths = Directory.GetDirectories(filePath);
 
-            for (int i = 0; i < filePaths.Length; i++)
+            if (!filePath.EndsWith(".txt"))
             {
-                Console.WriteLine((i+1).ToString() + ") " + filePaths[i]);
-            }
-            
-            int select = Convert.ToInt32(Console.ReadLine());
-            select = select--;
-          
-            foreach (string path in filePaths)
-            {
-                if ((filePaths[select-1]).Equals(path))
+                string[] filePaths = Directory.GetDirectories(filePath);
+
+                if (filePaths.Length > 0)
                 {
-                    
-                    return path;
+                    for (int i = 0; i < filePaths.Length; i++)
+                    {
+                        Console.WriteLine((i + 1).ToString() + ") " + filePaths[i]);
+                    }
+
+                    int select = (Convert.ToInt32(Console.ReadLine()));
+
+                    foreach (string path in filePaths)
+                    {
+                        if ((filePaths[select - 1]).Equals(path))
+                        {
+
+                            return path;
+                        }
+                    }
+                }
+                else
+                {
+                    filePaths = Directory.GetFiles(filePath, "*.txt");
+                    for (int i = 0; i < filePaths.Length; i++)
+                    {
+                        Console.WriteLine((i + 1).ToString() + ") " + filePaths[i]);
+                    }
+
+                    int select = (Convert.ToInt32(Console.ReadLine()));
+
+                    foreach (string path in filePaths)
+                    {
+                        if ((filePaths[select - 1]).Equals(path))
+                        {
+
+                            return path;
+                        }
+                    }
                 }
             }
-            return "hello";
+            else return "found";
+            return null;
         }
     }
 }
